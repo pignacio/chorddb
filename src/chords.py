ee070ea6da9432cc395fe7c6bf4888a7e66eea19
Ignacio Rossi <rossi.ignacio@gmail.com>
2014-05-10 23:58:50 -0300

    chords model
    * chords parsing
    * chord position extraction

---
+++ src/chords.py
@@ -0,0 +1,82 @@
+'''
+Created on May 10, 2014
+
+@author: ignacio
+'''
+import re
+import yaml
+import notes
+
+NOTES = list("ABCDEFG")
+NOTES_RE = "[{}]".format("".join(NOTES))
+
+ACCIDENTALS = ["b", "#"]
+ACCIDENTALS_RE = "{}".format("|".join(ACCIDENTALS))
+
+VARIATIONS = ["maj7", "m7", "m", "7", "dim", "aug", "sus4"]
+VARIATIONS_RE = "{}".format("|".join(VARIATIONS))
+
+CHORD_RE = "(({})({})?({})?)".format(NOTES_RE, ACCIDENTALS_RE, VARIATIONS_RE)
+STRICT_CHORD_RE = "^{}$".format(CHORD_RE)
+
+class Chord():
+    def __init__(self, note, accidental, variation):
+        note, accidental = notes.normalize(note, accidental)
+        self._note = note
+        self._accidental = accidental
+        self._variation = variation
+
+    @property
+    def note(self):
+        return self._note
+
+    @property
+    def accidental(self):
+        return self._accidental
+
+    @property
+    def variation(self):
+        return self._variation
+
+    def key(self):
+        return self._note + (self._accidental if self._accidental else "")
+
+    def text(self):
+        return "".join(x if x else "" for x in (self._note, self._accidental, self._variation))
+
+    def __str__(self):
+        return "Chord:{}".format(self.text())
+
+    @classmethod
+    def parse(cls, text):
+        mobj = re.search(STRICT_CHORD_RE, text)
+        if mobj:
+            note, accidental, chord = mobj.groups()[1:]
+            return cls(note, accidental, chord)
+        else:
+            raise ValueError("Couldn't parse Chord from '{}', CHORD_RE={}".format(text, CHORD_RE))
+
+    @classmethod
+    def extract_chordpos(cls, line):
+        index = 0
+        remainder = line
+        res = []
+        while True:
+            mobj = re.search(CHORD_RE, remainder)
+            if not mobj:
+                break
+            chord = mobj.group(1)
+            chordpos = remainder.index(chord)
+            res.append((Chord.parse(chord), chordpos + index))
+            movement = chordpos + len(chord)
+            remainder = remainder[movement:]
+            index += movement
+        return res
+
+    @classmethod
+    def all_chords(cls):
+        for key in notes.all_keys():
+            yield Chord.parse(key)
+            for variation in VARIATIONS:
+                yield Chord.parse(key + variation)
+
