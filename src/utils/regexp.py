e9a88c7f6710313624bf6d2db4ae86b2dbfdaf25
Ignacio Rossi <rossi.ignacio@gmail.com>
2014-05-17 13:42:10 -0300

    utils.regexp module

---
+++ src/utils/regexp.py
@@ -0,0 +1,36 @@
+'''
+Created on May 16, 2014
+
+@author: ignacio
+'''
+import re
+
+
+def strict(regexp):
+    if regexp.startswith("^") or regexp.endswith("$"):
+        raise ValueError("Regexp '{}' is already (semi)strict".format(regexp))
+    return "^{}$".format(regexp)
+
+
+class NoMatchError(ValueError):
+
+    def __init__(self, pattern, string):
+        ValueError.__init__("Could not match pattern:'{}' "
+                            "to string:'{}'".format(pattern, string))
+        self._pattern = pattern
+        self._string = string
+
+    @property
+    def pattern(self):
+        return self._pattern
+
+    @property
+    def string(self):
+        return self._string
+
+
+def re_search(pattern, string, flags=None):
+    mobj = re.search(pattern, string)
+    if not mobj:
+        raise NoMatchError(pattern, string)
+    return mobj
