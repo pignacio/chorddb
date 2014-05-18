c756a6e2407d7b2306b945b6fbaefd004b8c7ce8
Ignacio Rossi <rossi.ignacio@gmail.com>
2014-05-18 00:38:48 -0300

    chords.variations module

--- src/chords/__init__.py
+++ src/chords/__init__.py
@@ -9,9 +9,8 @@ import notes
 from notes import Key
 from utils.decorators import memoize
 from utils.regexp import strict
+from chords.variations import VARIATIONS_RE, VARIATIONS
 
-VARIATIONS = ["maj7", "m7", "m", "7", "dim", "aug", "sus4"]
-VARIATIONS_RE = "{}".format("|".join(VARIATIONS))
 
 CHORD_RE = "(({})({})?({})?)".format(notes.NOTES_RE, notes.ACCIDENTALS_RE,
                                      VARIATIONS_RE)
---
+++ src/chords/variations.py
@@ -0,0 +1,24 @@
+'''
+Created on May 17, 2014
+
+@author: ignacio
+'''
+from notes import Key
+
+
+_VARIATIONS_NOTES = {
+    None: ["A", "C#", "E"],
+    "7": ["A", "C#", "E", "G"],
+    "m": ["A", "C", "E"],
+    "m7": ["A", "C", "E", "G"],
+    "maj7": ["A", "C#", "E", "G#"],
+    "dim": ["A", "C", "Eb"],
+    "aug": ["A", "C#", "F"],
+    "sus4": ["A", "D", "E"],
+}
+VARIATIONS_NOTES = {v: [Key.parse(n).ord() for n in ns]
+                    for (v, ns) in _VARIATIONS_NOTES.items()}
+
+VARIATIONS = sorted((v for v in _VARIATIONS_NOTES if v), reverse=True)
+VARIATIONS_RE = "|".join(VARIATIONS)
+
