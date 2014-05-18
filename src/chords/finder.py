80c7ba8c2d898c3247cd067c837eace97d4429b2
Ignacio Rossi <rossi.ignacio@gmail.com>
2014-05-18 19:45:32 -0300

    ChordFinder class.  Simplified fingerings

--- src/chords/finder.py
+++ src/chords/finder.py
@@ -3,7 +3,7 @@ Created on May 17, 2014
 
 @author: ignacio
 '''
-from notes import KeyOctave
+from notes import KeyOctave, Key
 from chords.variations import VARIATIONS_NOTES
 import itertools
 from utils.decorators import memoize
@@ -47,32 +47,69 @@ class Instrument():
     def __len__(self):
         return len(self._keyoctaves)
 
-def _all_start_ends(size):
-    for start in xrange(size):
-        for end in xrange(start, size):
-            yield start, end + 1
+class ChordFinder():
+    def __init__(self, instrument, chord):
+        self._instrument = instrument
+        self._chord = chord
+        self._keys = chord.variation_keys()
+        self._res = []
+        self._string_notes = self._get_string_notes()
+        self._search()
+
+    def _search(self):
+        self._backtrack([])
+
+    def _backtrack(self, current):
+        if set(c[1].key for c in current if c) == self._keys:  # is valid
+            self._res.append(list(current))
+        if len(current) == len(self._string_notes):  # no childs
+            return
+
+        used = sorted([c[0] for c in current if c and c[0]])
+        maxi = used[-1] if used else 0
+        mini = used[0] if used else 10000
+
+        for pos, note in self._string_notes[len(current)]:
+            if maxi - pos > 4 or pos - mini > 4:  # Stretch basic test
+                continue
+            current.append((pos, note))
+            self._backtrack(current)
+            current.pop()
+
+        # Consider empty:
+        if all(c is None for c in current):
+            current.append(None)
+            self._backtrack(current)
+            current.pop()
+
+    def _fingerings(self):
+        for positions in self._res:
+            start = positions.count(None)
+            fingering = [x[0] for x in positions[start:]]
+            yield Fingering(fingering, self._instrument, start)
+
+
+
+    def _get_string_notes(self):
+        string_notes = []
+        for keyoctave in self._instrument.keyoctaves:
+            string_notes.append([])
+            for index in xrange(self._instrument.frets):
+                key = keyoctave.transpose(index)
+                if key.key in self._keys:
+                    string_notes[-1].append((index, key))
+        return string_notes
+
+    @classmethod
+    def find(cls, instrument, chord):
+        return cls(instrument, chord)._fingerings()
+
 
 def get_fingerings(chord, instrument, bass_check=True):
-    keys = set([chord.key.transpose(interval) for interval in VARIATIONS_NOTES[chord.variation]])
-    string_notes = []
-    for keyoctave in instrument.keyoctaves:
-        string_notes.append([])
-        for index in xrange(instrument.frets):
-            key = keyoctave.transpose(index)
-            if key.key in keys:
-                string_notes[-1].append(index)
-    for positions in itertools.product(*string_notes):
-        for start, end in _all_start_ends(len(instrument)):
-            if end - start < len(keys):
-                continue
-            fingering = Fingering(positions, instrument, start=start, end=end)
-            if max(fingering.positions()) - min(fingering.positions()) > 4:  # basic stretch test
-                continue
-            if len(set(ko.key for ko in fingering.keyoctaves())) != len(keys):  # full chord test
-                continue
-            if bass_check and fingering.bass().key != chord.key:  # bass note test
-                continue
-            yield fingering
+    for fingering in ChordFinder.find(instrument, chord):
+        if bass_check and fingering.bass().key != chord.key:  # bass note test
+            continue
+        yield fingering
 
 def _max(fingering):
     return max(x[0] for x in fingering if x[0] > 0)
@@ -82,48 +119,48 @@ def _min(fingering):
 
 
 class Fingering():
-    def __init__(self, positions, instrument, start=None , end=None):
+    def __init__(self, positions, instrument, start=None):
         self._instrument = instrument
         self._positions = positions
         self._start = start or 0
-        self._end = end or len(positions)
-        if not 0 <= self._start < len(positions):
+        if not 0 <= self._start < len(instrument):
             raise ValueError("Invalid start")
-        if not start < self._end <= len(positions):
-            raise ValueError("Invalid end")
-        if len(instrument) != len(positions):
+        if len(instrument) < start + len(positions):
             raise ValueError("Invalid positions for instrument")
         if not all(0 <= p < instrument.frets for p in positions):
             raise ValueError("Invalid positions for instrument")
 
-    @memoize
+    @property
     def positions(self):
-        return [self._positions[index] for index in xrange(self._start, self._end)]
+        return self._positions
 
     def keyoctaves(self):
-        return [ko.transpose(interval) for ko, interval in zip(self._instrument.keyoctaves, self._positions)[self._start:self._end]]
+        return [ko.transpose(interval) for ko, interval in zip(self._instrument.keyoctaves[self._start:], self._positions)]
 
     def bass(self):
         return min(self.keyoctaves())
 
     def penalty(self):
-        bar = min(self.positions())
-        indexed_poss = sorted(enumerate(x - bar for x in self.positions() if x > bar), key=lambda (string, pos): (pos, string), reverse=True)
+        bar = min(self._positions)
+        indexed_poss = sorted(enumerate(x - bar for x in self._positions if x > bar), key=lambda (string, pos): (pos, string), reverse=True)
         fingers = len(indexed_poss)
-        if len(indexed_poss) > 4 or (bar and fingers > 3):
+        if fingers > 4 or (bar and fingers > 3):
             return 10000
         penalty = self._start * 7 ** 2
-        penalty += (len(self._instrument) - self._end) * 8 ** 2
-        penalty += sum((p - bar + 2) ** 2 for p in self.positions())
+        penalty += (len(self._instrument) - self._start - len(self._positions)) * 8 ** 2
+        penalty += sum((p - bar + 2) ** 2 for p in self._positions)
         if bar:
             penalty += len(self._instrument) * 3 ** 2 + bar * 10
+        for a, b in zip(self._positions, self._positions[1:]):
+            if a and b:
+                penalty += (a - b) ** 2
         return penalty
 
 
     def _repr(self):
         res = ["x"] * len(self._instrument)
-        res[self._start:self._end] = self.positions()
-        return res
+        res[self._start:self._start] = self._positions
+        return res[:len(self._instrument)]
 
     def __str__(self):
         return "".join(map(str, self._repr()))
