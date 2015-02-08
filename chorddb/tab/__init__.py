20356c055a69e27d9af7fe28f7b92742a836f672
Ignacio Rossi <rossi.ignacio@gmail.com>
2015-02-08 13:23:24 -0300

    Fix transpose_tablature bug. + Tests

--- chorddb/tab/__init__.py
+++ chorddb/tab/__init__.py
@@ -14,14 +14,15 @@ from .parser import parse_tablature
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 
-def _transpose_line(line, interval):
+def transpose_line(line, interval):
     if line.type == 'chord':
         new_chords = [c._replace(chord=c.chord.transpose(interval))
-                      for c in line.chords]
-        return line._replace(chords=new_chords)
+                      for c in line.data.chords]
+        return line._replace(data=line.data._replace(chords=new_chords))
     else:
         return line
 
+
 def transpose_tablature(tablature, interval):
-    new_lines = [_transpose_line(line, interval) for line in tablature.lines]
+    new_lines = [transpose_line(line, interval) for line in tablature.lines]
     return tablature._replace(lines=new_lines)
---
+++ test/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+import logging
+
+
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+
