5f422601e36d15390b05bca8c171f80e22dadffd
Ignacio Rossi <rossi.ignacio@gmail.com>
2015-02-15 12:21:38 -0300

    Variations support multiple note sets

--- chorddb/chords/variations.py
+++ chorddb/chords/variations.py
@@ -6,20 +6,26 @@ Created on May 17, 2014
 from ..notes import Key
 
 
+def map_variations_to_intervals(variations):
+    return[
+        [Key.parse(note).ord() for note in variation]
+        for variation in variations]
+
+
+
 _VARIATIONS_NOTES = {
-    None: ["A", "C#", "E"],
-    "7": ["A", "C#", "E", "G"],
-    "m": ["A", "C", "E"],
-    "m7": ["A", "C", "E", "G"],
-    "maj7": ["A", "C#", "E", "G#"],
-    "dim": ["A", "C", "Eb"],
-    "aug": ["A", "C#", "F"],
-    "sus4": ["A", "D", "E"],
-    "m7": ["A", "C", "E", "G"],
-    "m7b5": ["A", "C", "Eb", "G"],
+    None: [["A", "C#", "E"]],
+    "7": [["A", "C#", "E", "G"]],
+    "m": [["A", "C", "E"]],
+    "m7": [["A", "C", "E", "G"]],
+    "maj7": [["A", "C#", "E", "G#"]],
+    "dim": [["A", "C", "Eb"]],
+    "aug": [["A", "C#", "F"]],
+    "sus4": [["A", "D", "E"]],
+    "m7b5": [["A", "C", "Eb", "G"]],
 }
-VARIATIONS_NOTES = {v: [Key.parse(n).ord() for n in ns]
-                    for (v, ns) in _VARIATIONS_NOTES.items()}
+VARIATIONS_NOTES = {v: map_variations_to_intervals(nss)
+                    for (v, nss) in _VARIATIONS_NOTES.items()}
 
 VARIATIONS = sorted((v for v in _VARIATIONS_NOTES if v), reverse=True)
 VARIATIONS_RE = "|".join(VARIATIONS)
